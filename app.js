57eb4ba3708386ff30f7655fc760997c806e1c8a
Arien Woodrow <woodrowa@bc.edu>
2018-05-21 17:08:28 -0400

    Using pug

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();//returns an express application; central part of app
 
-app.get('/', (request, response) =>{//slash is the first parameter(location paramenter) and anonymous callback function
-    response.send('I love Kimberly Dixon');
+app.set('view engine', 'pug');//tells express which templating engine to use
+
+app.get('/', (req, res) =>{//slash is the first parameter(location paramenter) and anonymous callback function
+    res.render('index');
 });
 
-app.listen(3000);
+app.get('/hello', (req, res) =>{//slash is the first parameter(location paramenter) and anonymous callback function
+    res.send('<h1>Hello, Javascript developer!</h1>');
+});
+
+app.listen(3000, ()=>{
+    console.log('The application is running on local port: 3000')
+});
