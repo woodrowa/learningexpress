3a8d8b43a72f195206ee511b067902d8c5323e31
Arien Woodrow <woodrowa@bc.edu>
2018-05-21 13:42:08 -0400

    added routes

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();//returns an express application; central part of app
 
+app.get('/', (request, response) =>{//slash is the first parameter(location paramenter) and anonymous callback function
+    response.send('I love Kimberly Dixon');
+});
+
 app.listen(3000);
