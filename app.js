884440caedb2f7e6ac6d37b9e79a80403f4a8306
Arien Woodrow <woodrowa@bc.edu>
2018-05-24 17:47:34 -0400

    leanig post requests

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();//returns an express application; central part of app
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('view engine', 'pug');//tells express which templating engine to use
 
@@ -10,6 +13,15 @@ app.get('/', (req, res) =>{//slash is the first parameter(location paramenter) a
 
 app.get('/cards', (req, res) =>{//slash is the first parameter(location paramenter) and anonymous callback function
     res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Look at the question."});
+});
+
+app.get('/hello', (req, res) =>{//get route serves the form
+    res.render('hello');
+});
+
+app.post('/hello', (req, res) =>{//to post the form data to this route
+    console.dir(req.body);
+    res.render('hello');
 });
 
 app.listen(3000, ()=>{
