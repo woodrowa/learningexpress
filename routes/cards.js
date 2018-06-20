d695ee754c949af30783a57abd7e11955ae3183e
Arien Woodrow <woodrowa@bc.edu>
2018-06-20 17:47:41 -0400

    More work on flashcards

--- routes/cards.js
+++ routes/cards.js
@@ -4,10 +4,22 @@ const data = require('../data/flashcardData.json').data;//data for flashcards
 const cards = data.cards;//access to cards data
 
 router.get('/:id', (req, res) =>{//slash is the first parameter(location paramenter) and anonymous callback function
-    res.render('card', {
-        prompt: cards[req.params.id].question,
-        hint: cards[req.params.id].hint
-    });
+    const side = req.query.side;// we're setting either the string "answer" or "question" to side from the query string
+    const id = req.params.id;//getting the card number from query string "id"
+    const text = cards[id][side];//we get either the answer or question from the flashcardData.json
+    const hint = cards[id].hint;//we can get the hint in the same way
+    const templateData = {text, id};//putting text and hint into templateData variable
+    
+    if (side === "question"){
+       templateData.hint = hint;//setting templateData's hint property to the hint variable if side equals question
+       templateData.sideToShow = "answer";
+       templateData.sideToShowDisplay = "Answer";
+    } else if(side === "answer"){
+        templateData.sideToShow = "question";
+        templateData.sideToShowDisplay = "Question";
+    };
+
+    res.render('card', templateData);//render card template with the data from json data 
 });//dont need to put '/cards' since path is in app.js
 
 module.exports = router;
